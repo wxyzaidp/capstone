406c747f3779f7c6602a274f90202107d6f071fa
Zaid Parkar <wyxzaid@Zaids-Laptop.local>
2025-03-30 14:45:18 -0400

    Refactor TimeService for improved date and time handling

--- ExacqMobileApp/App.tsx
+++ ExacqMobileApp/App.tsx
@@ -4,6 +4,7 @@ import { UI_COLORS } from './src/design-system/colors';
 import HomeScreen from './src/screens/HomeScreen';
 import AccessScreen from './src/screens/AccessScreen';
 import VisitorScreen from './src/screens/VisitorScreen';
+import MoreScreen from './src/screens/MoreScreen';
 import BottomNavigation from './src/components/BottomNavigation';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
@@ -132,8 +133,8 @@ const App = () => {
           />
         );
       case TABS.MORE:
-        // Not implemented yet, show home screen
-        return <HomeScreen />;
+        // Use our new MoreScreen component
+        return <MoreScreen onNavigateToHome={() => setActiveTab(TABS.HOME)} />;
       default:
         return <HomeScreen />;
     }
